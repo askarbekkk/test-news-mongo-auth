fd2b34c5318a237479242d9e89f34c000de00268
Azamat Zhamakeev <aza670.aj@gmail.com>
2021-10-20 15:02:23 +0600

    chalk

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 const express = require("express")
 const cors = require("cors")
 const morgan = require("morgan")
+const mongoose = require("mongoose")
+const chalk = require("chalk")
 require("dotenv").config()
 const authRouter = require("./routes/auth")
+const {connect} = require("mongoose");
 
 const server = express()
 
 server.use(cors())
 server.use(express.json())
 server.use(morgan())
+
+mongoose.connect(process.env.MONGO_URL)
+    .then(() => console.log(chalk.cyan("DB connected")))
+    .catch(() => console.log(chalk.red("DB not connected")))
+
 server.use("/api/v1", authRouter)
 
 server.listen(process.env.PORT, () =>{
