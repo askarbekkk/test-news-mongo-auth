fb70043f9a6a335c9a7e049ec9d5bed507f1f711
Azamat Zhamakeev <aza670.aj@gmail.com>
2021-10-20 17:34:28 +0600

    token

--- contoller/auth.js
+++ contoller/auth.js
@@ -1,9 +1,35 @@
+const Users = require("../models/authModel")
+const jwt = require("jsonwebtoken")
+
 const signUp = (req, res) =>{
-    res.json({message: "Its okay"})
+    const {name, email, password} = req.body
+    Users.findOne({email}).exec((err, user) =>{
+        if (user){
+            return res.status(400).json({error: "Email already use"})
+        }
+        let newUser = new Users({name, email, password})
+        newUser.save((err, savedUser) =>{
+            if (err){
+                return res.status(400).json({error: "Error to saved"})
+            }
+            return res.json({message: "User successfully sign up"})
+        })
+    })
 }
 
 const signIn = (req, res) =>{
-    res.json({message: "Its okay"})
+    const {email, password} = req.body
+    Users.findOne({email}).exec(async (error, user) =>{
+        if (error){
+          return   res.status(400).json({error: "User not found"})
+        }
+        const correctPassword = await user.authenticate(password)
+        if (!correctPassword){
+          return   res.status(400).json({error:"Email or password is not correct"})
+        }
+        const token = jwt.sign({_id: user._id}, process.env.SECRET_KEY, {expiresIn: "2d"})
+        res.json({token: token, user: {_id: user._id, name: user.name, email: user.email, role: user.role}})
+    })
 }
 
 module.exports = {signUp, signIn}
--- models/authModel.js
+++ models/authModel.js
@@ -1,9 +1,25 @@
 const mongoose = require("mongoose")
+const bcrypt = require("bcrypt")
 
 const userSchema = new mongoose.Schema({
     name: {type: String, trim: true, required: true, max: 28},
     email: {type: String, trim: true, required: true, unique: true, lowercase: true},
-    password: {type: String, trim: true, required: true}
+    password: {type: String, trim: true, required: true},
+    role: {type: String, default: "user"}
 }, {timestamps: true})
+
+userSchema.pre("save", function (next){
+    let user = this
+    bcrypt.hash(user.password, 10, function (err, hash){
+        if (err) return next(err)
+        user.password = hash
+        next()
+    })
+})
+
+userSchema.methods.authenticate = function (password) {
+    const hash_password = this.password
+    return  bcrypt.compare(String(password), hash_password)
+}
 
 module.exports = mongoose.model("users", userSchema)
