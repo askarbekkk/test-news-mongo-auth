682880ccc649e35f051bb0385dcbaf2299c26bd8
Azamat Zhamakeev <aza670.aj@gmail.com>
2021-10-22 20:23:21 +0600

    build

--- client/src/pages/Signup/index.js
+++ client/src/pages/Signup/index.js
@@ -22,7 +22,7 @@ const Signup = () => {
         e.preventDefault()
         axios({
             method: "POST",
-            url: "http://localhost:8000/api/v1/signup",
+            url: "/api/v1/signup",
             data: values
         }).then(({data}) => {
             setValues({name:"", email:"", password:""})
