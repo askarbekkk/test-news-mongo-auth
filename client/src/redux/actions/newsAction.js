f60c10314eafe1d5322ee98a312c076d925794b7
Azamat Zhamakeev <aza670.aj@gmail.com>
2021-11-03 14:59:51 +0600

    auth_token

--- client/src/redux/actions/newsAction.js
+++ client/src/redux/actions/newsAction.js
@@ -1,10 +1,12 @@
 import axios from "axios";
+import cookie from "js-cookie";
 
 
 export const getNews = () =>{
     return (dispatch) =>{
         dispatch({type: "NEWS_REQUEST"})
-        axios("http://localhost:8000/api/v1/news")
+        const headers = {"auth-token": cookie.get("token")}
+        axios("http://localhost:8000/api/v1/news", {headers})
             .then(({data}) => {
                 dispatch({type: "NEWS_SUCCESS", payload: data})
             })
--- routes/news.js
+++ routes/news.js
@@ -1,10 +1,10 @@
 const express = require("express")
 const {createNews, getAllNews, getOneNews} = require("../contoller/news");
-
+const verify = require("../middleware/authVerify")
 const router = express.Router()
 
 router.get("/:id", getOneNews)
-router.get("/", getAllNews)
+router.get("/",verify, getAllNews)
 router.post("/", createNews)
 
 
