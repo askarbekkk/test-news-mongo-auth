e111a8eea14c68bcaf4ee21eaab1b09f4a8ba31c
Azamat Zhamakeev <aza670.aj@gmail.com>
2021-11-10 08:57:19 +0600

    suspense

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, Suspense, lazy} from 'react';
 import {Router, Route, Switch} from "react-router-dom";
 import App from "./App";
 import Signup from "./pages/Signup";
@@ -30,17 +30,19 @@ const Routes = () => {
     }
     return (
         <Router history={history}>
-            <Switch>
-                <Route exact path='/' component={App}/>
-                <Route exact path='/signup' component={Signup}/>
-                <Route exact path='/signin' component={Signin}/>
-                <Route exact path='/news' component={News}/>
-                <Route exact path='/news_details/:id' component={NewsDetails}/>
-                <Route exact path='/user/:id' component={UserPage}/>
-                <PrivateRoute exact path='/add_news' component={AddNews}/>
-                <PrivateRoute exact path='/private' component={Private}/>
-                <AdminRoute exact path='/admin' component={Admin}/>
-            </Switch>
+           <Suspense fallback={<Spinner />}>
+               <Switch>
+                   <Route exact path='/' component={App}/>
+                   <Route exact path='/signup' component={Signup}/>
+                   <Route exact path='/signin' component={Signin}/>
+                   <Route exact path='/news' component={News}/>
+                   <Route exact path='/news_details/:id' component={NewsDetails}/>
+                   <Route exact path='/user/:id' component={UserPage}/>
+                   <PrivateRoute exact path='/add_news' component={AddNews}/>
+                   <PrivateRoute exact path='/private' component={Private}/>
+                   <AdminRoute exact path='/admin' component={Admin}/>
+               </Switch>
+           </Suspense>
         </Router>
     );
 };
